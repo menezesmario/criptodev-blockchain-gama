e364f9cc4cc40399468088d5c5e62cb2db4fbf3e
Mário Menezes <mario082@gmail.com>
2022-05-21 11:11:20 -0300

    fix conversor test

--- dia-04/rgb-hex/tests/conversor.test.js
+++ dia-04/rgb-hex/tests/conversor.test.js
@@ -15,16 +15,12 @@ describe("Hexadecimal to RGB conversion", () => {
 
     it("Color in hexadecimal is #FF0000", () => {})
         assert.equal(hexToRgb('#ff0000'), '255,0,0')
- 
-
-}
-)
-
+})
 
 describe("Hexadecimal to RGB conversion Error", () => { 
 
     it("Error", () => {})
     assert.notEqual(hexToRgb('#ff0000'),'255,0,3')
-    }  
-)
+    
+})
 
