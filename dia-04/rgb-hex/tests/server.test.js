4fce9d0fee1ac9d0a98e95d68f6df08ff28e8d7c
Mário Menezes <mario082@gmail.com>
2022-05-21 11:10:59 -0300

    server test

---
+++ dia-04/rgb-hex/tests/server.test.js
@@ -0,0 +1,23 @@
+const { assert, expect } = require('chai')
+const request = require("request");
+
+
+describe("Color Converter server", () => {
+
+    describe("RGB to Hex", () => {
+
+      it("status 200", () => {});
+
+      it("color in hex", () => {});
+    });
+  
+    describe("Hex to RGB", () => {
+
+      it("status 200", () => {});
+
+      it("color in RGB", () => {});
+    });
+  
+  });
+
+
