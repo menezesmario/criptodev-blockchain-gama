3970269f83ad7b238bae0261f1326cb8bae15f0c
Mário Menezes <mario082@gmail.com>
2022-05-21 15:52:34 -0300

    fix hexToRgb

--- dia-04/rgb-hex/conversor.js
+++ dia-04/rgb-hex/conversor.js
@@ -7,13 +7,18 @@ const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
 //   console.log(rgbToHex(255, 0, 0)); // #FF05FF
   
 
-const hexToRgb = (hex) =>
-hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+const hexToRgb = (hex) => {
+
+const array = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
            ,(m, r, g, b) => '#' + r + r + g + g + b + b)
   .substring(1).match(/.{2}/g)
-  .map(x => parseInt(x, 16))
-// console.log(hexToRgb("#11AB24")) // [17, 171, 36]
-// console.log(hexToRgb("#A13")) // [170, 17, 51]
 
+  if(array) {
+  return array.map(x => parseInt(x, 16))
+  console.log(hexToRgb("#11AB24")) // [17, 171, 36]
+// console.log(hexToRgb("#A13")) // [170, 17, 51]
+}
+}
+console.log(hexToRgb("#11AB24"))
 
 module.exports = { rgbToHex, hexToRgb }
