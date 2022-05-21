013d5fe73e080784c7d6b39203de60a24d04b169
Mário Menezes <mario082@gmail.com>
2022-05-21 15:53:27 -0300

    queries ok

--- dia-04/rgb-hex/server/server.js
+++ dia-04/rgb-hex/server/server.js
@@ -4,11 +4,31 @@ const port = 8080;
 
 const app = express()
 
+const {hexToRgb, rgbToHex} = require('../conversor')
+
+
+
 app.get('/status', (req, res) => {
   res.send('Servidor Ok')
 })
 
 
+
+app.get(`/rgbToHex`, (req, res) => {  
+  let r = parseInt(req.query.r)  
+  let g = parseInt(req.query.g)
+  let b = parseInt(req.query.b)
+  console.log(r,g,b)
+  console.log(rgbToHex(r,g,b))
+  res.send(rgbToHex(r,g,b))
+})
+
+app.get('/hexToRgb', (req, res) => {
+  let hex = req.query.hex
+  let rgb = hexToRgb(hex)
+  res.send(hexToRgb(hex).toString())
+  console.log((hexToRgb(hex)))
+})
 
 app.listen(port, () => {
   console.log(`Servidor Ok porta: ${port}`)
