4e31ed9469f68fa9076b54c7a1e766d25988e0cd
Mário Menezes <mario082@gmail.com>
2022-05-28 21:03:08 -0300

    chain

---
+++ dia-10/src/chain.js
@@ -0,0 +1,27 @@
+const Block = require('./block')
+
+class Chain {
+
+    constructor() {
+        this.instance = [new Block({index: 0, previousHash: 0, data: 'Genesis Block'})]
+        this.index = 1
+    }
+
+
+    addBlock(data) {
+        const index = this.index
+        const previousHash = this.instance[this.index - 1].hash
+
+        const block = new Block({index, previousHash, data})
+
+        this.index++
+        this.instance.push(block)f
+    }
+
+    print() {
+        console.log(this.instance)
+    }
+
+}
+
+module.exports = Chain
